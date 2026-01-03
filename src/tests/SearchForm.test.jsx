81d451a6c812207023a81c1b5b5659f471a0ed92
thinara <thinarasenarathne@gmail.com>
2026-01-03 07:52:25 -0800

    fic searchform test configeration

--- src/tests/SearchForm.test.jsx
+++ src/tests/SearchForm.test.jsx
@@ -4,4 +4,4 @@ import SearchForm from "../components/SearchForm"
 test("postcode filter input renders", () => {
   render(<SearchForm properties={[]} setResults={jest.fn()} />)
   expect(screen.getByPlaceholderText(/BR1/i)).toBeInTheDocument()
-})
+})  
